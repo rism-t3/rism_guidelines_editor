8594dbb16ea3ca9bf6e52850f07055666107f1d9
stephan <stephan@hirsch-online.com>
2016-02-26 10:42:49 +0100

    Added rough tests

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root :to => redirect('/admin/documents#index')
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # The priority is based upon order of creation: first created -> highest priority.
